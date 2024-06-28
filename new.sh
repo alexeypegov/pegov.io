0d0ff0b79b413d6c43f2a09927776f7cb37ecbee
Alexey Pegov <alexey@pegov.dev>
2024-06-28 13:54:57 +0200

    new.sh

---
+++ new.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+if  [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: new.sh post|tweet|video post-slug"
+  exit 1
+fi
+
+TYPE="$1"
+SLUG="$2"
+
+RED='\033[0;31m'
+RESET='\033[0m'
+ITALIC='\033[3m'
+
+filename=$(printf "posts/%s-%s.md" $(date +'%Y-%m-%d') $SLUG)
+
+if [ -e "$filename" ]; then
+  echo "${RED}ERROR:${RESET} ${ITALIC}${filename}${RESET} already exist!"
+  exit 1
+fi
+
+if [ $TYPE = "post" ]; then
+  cat <<EOF | sed 's/^[ ]*//' > "$filename"
+  ---
+  Title:
+  Date: $(date +'%Y-%m-%d')
+  Tags:
+  Summary:
+  Cover:
+  ---
+
+EOF
+elif [ $TYPE = "tweet" ]; then
+  cat <<EOF | sed 's/^[ ]*//' > "$filename"
+  ---
+  Title:
+  Date: $(date +'%Y-%m-%d')
+  Tags:
+  View: tweet
+  Tweet: 
+  ---
+
+EOF
+elif [ $TYPE = "video" ]; then
+  cat <<EOF | sed 's/^[ ]*//' > "$filename"
+  ---
+  Title:
+  Tags:
+  View: video
+  Video:
+  ---
+
+EOF
+fi
+
+# echo "${filename} ${TYPE} has been created"
+
+hx ${filename}
